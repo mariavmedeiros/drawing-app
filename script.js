24bee508d9535d6a3690c149e02383cf7a756977
Maria Medeiros <maitevelori@hotmail.com>
2022-12-19 19:00:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext("2d");
 
 let size = 20;
 let color = "black";
+let x;
+let y;
 
 function drawCircle(x, y) {
   ctx.beginPath();
@@ -20,6 +22,3 @@ function drawLine(x1, y1, x2, y2) {
   ctx.lineWidth = size;
   ctx.stroke();
 }
-
-drawCircle(100, 200);
-drawLine(300, 300, 300, 500);
